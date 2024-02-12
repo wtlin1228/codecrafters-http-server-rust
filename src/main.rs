e128e2de47caad705cce05c941969050112dfe30
wtlin1228 <wtlin1228@gmail.com>
2024-02-12 23:00:44 +0800

    pass 5th stage

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,17 @@ fn main() -> anyhow::Result<()> {
         match stream {
             Ok(mut stream) => {
                 let mut reader = BufReader::new(&stream);
-                let mut line = vec![];
-                reader
-                    .read_until(b'\n', &mut line)
-                    .context("read until \\n")?;
-                let request = HttpRequest::new(&line).context("parse HTTP request")?;
+                let request = HttpRequest::new(&mut reader).context("parse HTTP request")?;
                 match &request.path[..] {
                     "/" => respond_with_200_ok(&mut stream)?,
                     s if s.starts_with("/echo/") => {
                         let random_string = &s["/echo/".len()..];
                         respond_with_text_content(&mut stream, random_string)
                             .context("echo with input string")?;
+                    }
+                    s if s.starts_with("/user-agent") => {
+                        respond_with_text_content(&mut stream, &request.user_agent)
+                            .context("respond with user agent")?;
                     }
                     _ => respond_with_404_not_found(&mut stream)?,
                 }
@@ -60,23 +60,42 @@ fn respond_with_404_not_found(stream: &mut TcpStream) -> anyhow::Result<()> {
 }
 
 #[allow(dead_code)]
+#[derive(Debug, Default)]
 struct HttpRequest {
     http_method: String,
     path: String,
     http_version: String,
+    host: String,
+    user_agent: String,
 }
 
 impl HttpRequest {
-    fn new(line: &Vec<u8>) -> anyhow::Result<Self> {
-        let line = std::str::from_utf8(line).context("validate input as a valid utf8 string")?;
+    fn new(reader: &mut BufReader<&TcpStream>) -> anyhow::Result<Self> {
+        let mut request = Self::default();
+        let mut lines_iter = reader
+            .lines()
+            .map(|l| l.context("read one line as string").unwrap());
+
+        // GET /user-agent HTTP/1.1
+        let line = lines_iter.next().context("read 1st line")?;
         let mut splitted_line = line.split(' ');
-        Ok(Self {
-            http_method: splitted_line.next().context("get HTTP method")?.to_string(),
-            path: splitted_line.next().context("get path")?.to_string(),
-            http_version: splitted_line
-                .next()
-                .context("get HTTP version")?
-                .to_string(),
-        })
+        request.http_method = splitted_line.next().context("get HTTP method")?.to_string();
+        request.path = splitted_line.next().context("get path")?.to_string();
+        request.http_version = splitted_line
+            .next()
+            .context("get HTTP version")?
+            .to_string();
+
+        // Host: localhost:4221
+        let line = lines_iter.next().context("read 2nd line")?;
+        let mut splitted_line = line.split(' ');
+        request.host = splitted_line.nth(1).context("get host")?.to_string();
+
+        // User-Agent: curl/7.64.1
+        let line = lines_iter.next().context("read 3rd line")?;
+        let mut splitted_line = line.split(' ');
+        request.user_agent = splitted_line.nth(1).context("get user agent")?.to_string();
+
+        Ok(request)
     }
 }
