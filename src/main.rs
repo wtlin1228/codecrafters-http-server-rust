2c76df0f4664683c2891327183fb0c4f835ad270
wtlin1228 <wtlin1228@gmail.com>
2024-02-12 20:20:46 +0800

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                let response = "HTTP/1.1 200 OK\r\n\r\n";
+                stream.write(response.as_bytes())?;
+                stream.flush()?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    anyhow::Ok(())
 }
